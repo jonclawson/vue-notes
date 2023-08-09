401544d5e7653f82312fb742c93b8fcbb2a10dfe
jonclawson <30538332+jonclawson@users.noreply.github.com>
2023-08-08 23:48:00 -0700

    2 files modified, 1 added

--- src/components/List.vue
+++ src/components/List.vue
@@ -4,15 +4,25 @@
       class="list-group-item d-flex justify-content-between"
       v-for="item in items"
     >
-      <Button @clickButton="() => $emit('clickItem', item)" 
+      <Button @clickButton="() => $emit('clickItem', item)"
         >{{ item.id }} {{ item.message }}
       </Button>
-      <Button
-        @clickButton="() => removeNote(item)"
-        className="btn-sm btn-danger"
-        name="x"
-        type="button"
-      />
+
+      <span>
+        <Button
+          @clickButton="() => $emit('clickItem', item)"
+          className="btn-sm btn-warning"
+        >
+          <Pencil />
+        </Button>
+        <Button
+          @clickButton="() => removeNote(item)"
+          className="btn-sm btn-danger"
+          type="button"
+        >
+          X</Button
+        >
+      </span>
     </li>
   </ul>
 </template>
@@ -20,10 +30,12 @@
 <script>
 import { useNoteStore } from '../store';
 import Button from './Button.vue';
+import Pencil from './Pencil.vue';
 export default {
   name: 'List',
   components: {
     Button,
+    Pencil,
   },
   props: {
     items: Object,
--- src/main.js
+++ src/main.js
@@ -3,7 +3,6 @@ import { createApp } from 'vue';
 import App from './App.vue';
 import 'bootstrap/dist/css/bootstrap.css';
 import 'bootstrap/dist/js/bootstrap.js';
-
 import { createPinia } from 'pinia';
 const pinia = createPinia();
 
